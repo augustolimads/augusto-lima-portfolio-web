e094cebae3f3ddefb5026bc553ed908371570405
augustolimads <augustolimads@gmail.com>
2021-03-02 15:19:06 -0300

    update posts description

--- src/Content/posts.ts
+++ src/Content/posts.ts
@@ -5,8 +5,8 @@ const posts = [
 		type: "portfolio",
 		title: "Calculadora de finanças para casal",
 		date: "05/12/2020",
-		content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Proin cursus dui eu turpis cursus.`,
+		content: `Projeto no qual sugere para o casal uma divisão de contas 
+		proporcional ao salário.`,
 		img: "images/app-casal.png",
 		labels: ["nextJS", "ReactJS", "TailwindCSS"],
 	},
@@ -16,10 +16,10 @@ const posts = [
 		type: "portfolio",
 		title: "Animelovers - lista de animes",
 		date: "03/08/2020",
-		content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Proin cursus dui eu turpis cursus.`,
+		content: `Nesse projeto o front-end consome essa API: https://api.jikan.moe/v3/,
+		para listar os animes de acordo com a pesquisa.`,
 		img: "images/animelovers.png",
-		labels: ["nextJS", "ReactJS", "TailwindCSS"],
+		labels: ["nextJS", "ReactJS", "TailwindCSS", "API"],
 	},
 	{
 		id: 3,
@@ -27,8 +27,8 @@ const posts = [
 		type: "portfolio",
 		title: "Digipet Emoji - pet virtual",
 		date: "11/01/2020",
-		content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Proin cursus dui eu turpis cursus.`,
+		content: `Jogo feito 100% com funcionalidades do javascript, 
+		sem bibliotecas externas.`,
 		img: "images/digipet.png",
 		labels: ["HTML", "CSS", "Javascript"],
 	},
