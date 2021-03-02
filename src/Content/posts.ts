23efcd13e9fd70ad815f48d6722b80ab2d5a7d8d
augustolimads <augustolimads@gmail.com>
2021-03-02 12:10:34 -0300

    add some projects

--- src/Content/posts.ts
+++ src/Content/posts.ts
@@ -23,6 +23,28 @@ const posts: Posts = [
 		img: "images/app-casal.png",
 		labels: ["nextJS", "ReactJS", "TailwindCSS"],
 	},
+	{
+		id: 2,
+		link: "https://animelovers-gamma.vercel.app/",
+		type: "portfolio",
+		title: "Animelovers - lista de animes",
+		date: "03/08/2020",
+		content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Proin cursus dui eu turpis cursus.`,
+		img: "images/animelovers.png",
+		labels: ["nextJS", "ReactJS", "TailwindCSS"],
+	},
+	{
+		id: 3,
+		link: "https://augustolimads.github.io/digipet-emoji/",
+		type: "portfolio",
+		title: "Digipet Emoji - pet virtual",
+		date: "11/01/2020",
+		content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Proin cursus dui eu turpis cursus.`,
+		img: "images/digipet.png",
+		labels: ["HTML", "CSS", "Javascript"],
+	},
 ];
 
 export default posts;
