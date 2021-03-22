c4aeb7d604f0359efd59e73a2b65015ac37fa106
Augusto Lima <augustolimads@gmail.com>
2021-03-22 13:18:19 -0300

    🎨 update logo color

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 import style from '../styles/components/Logo.module.css';
 
 export default function Logo() {
     return (
-        <img src="icons/logo.svg" alt="logo" className={style.logo} />
+        <img src="/icons/logo.svg" alt="logo" className={style.logo} />
     )
 }
